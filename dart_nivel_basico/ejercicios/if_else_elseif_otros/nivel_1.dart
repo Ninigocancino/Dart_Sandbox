e8ecf5a760d323a98f96969acc1c4fe7b80fe08f
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-11-17 22:09:58 -0600

    In the 'if_else_elseif_otros' directory, the 'nivel_1.dart' file was created, and the first exercise was added to the file

---
+++ dart_nivel_basico/ejercicios/if_else_elseif_otros/nivel_1.dart
@@ -0,0 +1,17 @@
+//Ejemplo de Nivel 1
+
+void main() {
+  int numero = 5;
+
+  int esc = 8;
+
+
+  if (numero > 0) {
+    print("El número es positivo");
+  }
+
+  if (esc >= 6) {
+    print("Es un número mayor que 6");
+  }
+}
+
