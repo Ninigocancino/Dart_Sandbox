51a938d715ddb2452277dc80605dbf3d032d8b63
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-11-18 17:43:03 -0600

    New exercises were added to the 'nivel_1.dart' file

--- dart_nivel_basico/ejercicios/if_else_elseif_otros/nivel_1.dart
+++ dart_nivel_basico/ejercicios/if_else_elseif_otros/nivel_1.dart
@@ -1,6 +1,10 @@
-//Ejemplo de Nivel 1
+
 
 void main() {
+  
+  //Ejemplo de Nivel 1
+
+  /*
   int numero = 5;
 
   int esc = 8;
@@ -13,5 +17,42 @@ void main() {
   if (esc >= 6) {
     print("Es un número mayor que 6");
   }
+  */
+   
+
+  /*
+  //Ejercicio dos: validar si un numero es par o no
+  int numero = 4;
+
+  if (numero %2 == 0){
+    print("El numero es par");
+  }else {
+    print("es impar");
+  }
+  */
+
+  //Ejercicio 2: Evaluar si un número es positivo o negativo
+  /*
+  int numero = -3;
+
+  if (numero > 0){
+    print("el numero es positivo");
+  } else if(numero == 0){
+    print("es cero");
+  } else {
+    print("es negativo");
+  }
+  */
+
+  //Ejercicio 3: Evaluación de edad
+
+  int edad = 18;
+
+  if (edad >= 18){
+    print("eres mayor de edad");
+  } else {
+    print("eres menor de edad");
+  }
 }
+
 
