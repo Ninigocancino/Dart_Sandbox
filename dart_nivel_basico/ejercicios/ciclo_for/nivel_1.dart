db5b7da8d4e3e5765a804afd57b7f3034f22e10d
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-11-20 20:59:53 -0600

    The 'ciclo_for' directory was created, and the file 'nivel_1.dart' was added to the directory

---
+++ dart_nivel_basico/ejercicios/ciclo_for/nivel_1.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  /*---------------------Ejemplo de nivel básico------------------
+
+  // Bucle for básico que cuenta hasta 5
+
+  // después de la sentencia 'for'
+  // el fragmento 'int i = 1;' inicializa la variable de control i con el valor 1.
+  // el fragmento de código 'i <= 5' correponde a la condición que se evalúa antes de cada iteración. El bucle continuará mientras 'i' sea menor o igual a 5.
+  // Finalmente, el fragmento de código  'i++' hace que 'i' se incremente después de cada iteración.
+
+  
+  for (int i = 1; i <= 5; i++) {
+    print('Número: $i');
+  }
+
+  */
+}
