303772cab75d37c85f2a1b81f8e63632749c2936
Victor Cancino <11082428+Ninigocancino@users.noreply.github.com>
2023-11-20 21:34:13 -0600

    The exercises from 1 to 3 were added to the 'nivel_1.dart' file

--- dart_nivel_basico/ejercicios/ciclo_for/nivel_1.dart
+++ dart_nivel_basico/ejercicios/ciclo_for/nivel_1.dart
@@ -9,10 +9,50 @@ void main() {
   // el fragmento de código 'i <= 5' correponde a la condición que se evalúa antes de cada iteración. El bucle continuará mientras 'i' sea menor o igual a 5.
   // Finalmente, el fragmento de código  'i++' hace que 'i' se incremente después de cada iteración.
 
-  
+
   for (int i = 1; i <= 5; i++) {
     print('Número: $i');
   }
 
   */
+
+
+  /*
+  //-------------------Ejercicio 1: Suma de numeros pares----------------
+
+  //Escribe un programa en Dart que utilice un bucle for para calcular la suma de los primeros 10 números pares. Imprime el resultado al final.
+
+  int suma = 0;
+
+  for (int i = 2; i <=20; i +=2 ) {
+    suma += i;
+  }
+  print("La suma de los primeros 10 numeros pares es: $suma");
+
+  */
+
+
+  /*
+  //-------------------Ejercicio 2: Tabla de Multiplicar---------------
+  
+  //Escribe un programa en Dart que utilice un bucle for para imprimir la tabla de multiplicar del 5. Imprime los primeros 10 múltiplos del 5.
+
+
+  for (int i = 1; i <= 10; i++) {
+    print('5 x $i = ${5 * i}');
+    }
+
+  */
+
+
+
+  //------------------Ejercicio 3: Contador Regresivo---------------
+
+  //Escribe un programa en Dart que utilice un bucle for para imprimir un contador regresivo desde 10 hasta 1.
+
+  for (int i = 10; i >= 1; i--) {
+    print('Contador: $i');
+  }
+
+
 }
